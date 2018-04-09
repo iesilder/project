1e2149e66760bd37c33c5f817cc1556b19fbb676
TaeSun Yoo <yts8317@gmail.com>
2018-04-09 13:49:39 +0900

    MongoDB까지 출력

--- project/src/com/webjjang/board/controller/DataController.java
+++ project/src/com/webjjang/board/controller/DataController.java
@@ -26,7 +26,7 @@ import com.webjjang.util.MongoDBUtil;
 public class DataController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	public static String pre = "/WEB-INF/views/";
+	public static String pre = "/WEB-INF/data/";
 	public static String suf = ".jsp";
     /**
      * @see HttpServlet#HttpServlet()
@@ -41,10 +41,10 @@ public class DataController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 
 		// Servlet 접속 확인
-		System.out.println("BoardController.service()");
+//		System.out.println("BoardController.service()");
 		
 		// 데이터 처리 및 request에 담기 - 미완
 		MongoDatabase mongoDatabase = MongoDBUtil.getConnection("seoulpopulation");
@@ -52,23 +52,26 @@ public class DataController extends HttpServlet {
 		MongoIterable<Document> iterable = mongoCollection.find();
 		MongoCursor<Document> mongoCursor = iterable.iterator();
 		List<Document> list = new ArrayList<Document>();
-		System.out.println("객체 생성");
+//		System.out.println("객체 생성");
 		while(mongoCursor.hasNext()) {
 			Document document = mongoCursor.next();
 			list.add(document);
 		}
-		System.out.println("리스트 담기");
+//		System.out.println("리스트 담기");
 		request.setAttribute("list", list);
-		System.out.println("리퀘스트에 담기");
+//		System.out.println("리퀘스트에 담기");
 		
 		// uri만들기
-		System.out.println(request.getMethod());
-		System.out.println(request.getServletPath());
+//		System.out.println(request.getMethod());
+//		System.out.println(request.getServletPath());
 		String uri = request.getRequestURI();
-		System.out.println(uri);
+//		System.out.println(uri);
 		String res = uri.substring(uri.indexOf(request.getServletPath()));
-		System.out.println(res);
-		String jsp = pre+res.substring(1,res.lastIndexOf("."))+suf;
+//		System.out.println(res);
+//		String test = res.substring(1);
+//		System.out.println(test);
+		String jsp = pre+res.substring(1)+suf;
+//		System.out.println(jsp);
 		RequestDispatcher dispatcher = request.getRequestDispatcher(jsp);
 		dispatcher.forward(request, response);
 	}
