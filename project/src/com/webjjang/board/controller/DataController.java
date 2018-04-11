ae200014f018ac095cfb4dbd261f873fcfdff6f8
TaeSun Yoo <yts8317@gmail.com>
2018-04-11 19:47:37 +0900

    20180411

--- project/src/com/webjjang/board/controller/DataController.java
+++ project/src/com/webjjang/board/controller/DataController.java
@@ -1,33 +1,19 @@
 package com.webjjang.board.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.bson.Document;
-
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
-import com.webjjang.util.MongoDBUtil;
-
 /**
  * Servlet implementation class DataController
  */
-//@WebServlet("/DataController")
+@WebServlet("/DataController")
 public class DataController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	public static String pre = "/WEB-INF/data/";
-	public static String suf = ".jsp";
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -41,39 +27,15 @@ public class DataController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
-		// Servlet 접속 확인
-//		System.out.println("BoardController.service()");
-		
-		// 데이터 처리 및 request에 담기 - 미완
-		MongoDatabase mongoDatabase = MongoDBUtil.getConnection("seoulpopulation");
-		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("earn");
-		MongoIterable<Document> iterable = mongoCollection.find();
-		MongoCursor<Document> mongoCursor = iterable.iterator();
-		List<Document> list = new ArrayList<Document>();
-//		System.out.println("객체 생성");
-		while(mongoCursor.hasNext()) {
-			Document document = mongoCursor.next();
-			list.add(document);
-		}
-//		System.out.println("리스트 담기");
-		request.setAttribute("list", list);
-//		System.out.println("리퀘스트에 담기");
-		
-		// uri만들기
-//		System.out.println(request.getMethod());
-//		System.out.println(request.getServletPath());
-		String uri = request.getRequestURI();
-//		System.out.println(uri);
-		String res = uri.substring(uri.indexOf(request.getServletPath()));
-//		System.out.println(res);
-//		String test = res.substring(1);
-//		System.out.println(test);
-		String jsp = pre+res.substring(1)+suf;
-//		System.out.println(jsp);
-		RequestDispatcher dispatcher = request.getRequestDispatcher(jsp);
-		dispatcher.forward(request, response);
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
--- project/src/com/webjjang/util/MongoDBUtil.java
+++ project/src/com/webjjang/util/MongoDBUtil.java
@@ -10,12 +10,16 @@ public class MongoDBUtil {
 	private static int port = 27017;
 	private static String hostName = "localhost";
 
-//	// uri를 이용하여 접속 시
-//	private static String userId = "admin"; // mlab에 접속해서 만든 아이디가 아닌 dababase->user에서 생성한 아이디
-//	private static String userPw = "1234"; // mlab에 접속해서 만든 아이디가 아닌 dababase->user에서 생성한 비밀번호
-//	private static String userURI = "@ds029715.mlab.com:29715/seoulpopulation"; // URI의 @부터 끝까지
-//	// 데이터베이스에 접속 할 때 필요한 user id와 pw를 uri문장에 합쳐준다.
-//	private static String mlabUri = "mongodb://" + userId + ":" + userPw + userURI;
+	// // uri를 이용하여 접속 시
+	// private static String userId = "admin"; // mlab에 접속해서 만든 아이디가 아닌
+	// dababase->user에서 생성한 아이디
+	// private static String userPw = "1234"; // mlab에 접속해서 만든 아이디가 아닌
+	// dababase->user에서 생성한 비밀번호
+	// private static String userURI = "@ds029715.mlab.com:29715/seoulpopulation";
+	// // URI의 @부터 끝까지
+	// // 데이터베이스에 접속 할 때 필요한 user id와 pw를 uri문장에 합쳐준다.
+	// private static String mlabUri = "mongodb://" + userId + ":" + userPw +
+	// userURI;
 
 	// // 접속을 시도 할 데이터 베이스의 정보
 	// private static String databaseName = "seoulpopulation";
@@ -23,20 +27,20 @@ public class MongoDBUtil {
 	// close를 안하여 나타나는 오류를 없애기 위해 만든 외부 객체
 	// 로컬 접속 시 사용할 MongoClient 객체
 	static MongoClient mongoClient = new MongoClient(hostName, port);
-//	// mLab을 이용하여 접속 할 때 이용할 uri와 MongoClient 객체
-//	static MongoClientURI uri = new MongoClientURI(mlabUri);
-//	static MongoClient mongoClient1 = new MongoClient(uri);
+	// // mLab을 이용하여 접속 할 때 이용할 uri와 MongoClient 객체
+	// static MongoClientURI uri = new MongoClientURI(mlabUri);
+	// static MongoClient mongoClient1 = new MongoClient(uri);
 
 	// 접속 시 필요한 자료
 	// 접속 시 databaseName을 파라메터로 넘겨서 접속한다.
-	public static MongoDatabase getConnection(String databaseName) {
+	public static MongoDatabase getConnection(String databaseName) throws Exception {
 		return mongoClient.getDatabase(databaseName);
 	}// end of getConnection()
 
 	// URI 접속 시 필요한 자료
 	// 접속 시 databaseName을 파라메터로 넘겨서 접속한다.
-//	public static MongoDatabase getConnectionToMlab() {
-//		return mongoClient1.getDatabase(uri.getDatabase());
-//	}// end of getConnectionToMlab()
+	// public static MongoDatabase getConnectionToMlab() {
+	// return mongoClient1.getDatabase(uri.getDatabase());
+	// }// end of getConnectionToMlab()
 
 }
