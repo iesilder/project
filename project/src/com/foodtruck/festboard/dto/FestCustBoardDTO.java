9794b11679f135e780fd39453da85ed244256931
hong <hong@401PC-B1>
2018-04-12 13:44:56 +0900

    행사

---
+++ project/src/com/foodtruck/festboard/dto/FestCustBoardDTO.java
@@ -0,0 +1,107 @@
+package com.foodtruck.festboard.dto;
+
+public class FestCustBoardDTO {
+
+	private int custno;
+	private String applyname, applytel;
+	private int applyno;
+	private String applyloc, applytime;
+	private int hit;
+
+	// 초기화 블럭
+	public FestCustBoardDTO() {
+
+	}
+
+	// 행사일정 리스트용
+	public FestCustBoardDTO(int custno, String applyname, String applytel, int hit) {
+		this.custno = custno;
+		this.applyname = applyname;
+		this.applytel = applytel;
+		this.hit = hit;
+	}
+
+	// 행사일정 글보기용
+	public FestCustBoardDTO(int custno, String applyname, String applytel, int applyno, String applyloc,
+			String applytime, int hit) {
+		this.custno = custno;
+		this.applyname = applyname;
+		this.applytel = applytel;
+		this.applyno = applyno;
+		this.applyloc = applyloc;
+		this.applytime = applytime;
+		this.hit = hit;
+	}
+
+	// 행사일정 글등록 및 수정용
+	public FestCustBoardDTO(String applyname, String applytel, int applyno, String applyloc, String applytime) {
+		this.applyname = applyname;
+		this.applytel = applytel;
+		this.applyno = applyno;
+		this.applyloc = applyloc;
+		this.applytime = applytime;
+	}
+
+	public int getCustno() {
+		return custno;
+	}
+
+	public void setCustno(int custno) {
+		this.custno = custno;
+	}
+
+	public String getApplyname() {
+		return applyname;
+	}
+
+	public void setApplyname(String applyname) {
+		this.applyname = applyname;
+	}
+
+	public String getApplytel() {
+		return applytel;
+	}
+
+	public void setApplytel(String applytel) {
+		this.applytel = applytel;
+	}
+
+	public int getApplyno() {
+		return applyno;
+	}
+
+	public void setApplyno(int applyno) {
+		this.applyno = applyno;
+	}
+
+	public String getApplyloc() {
+		return applyloc;
+	}
+
+	public void setApplyloc(String applyloc) {
+		this.applyloc = applyloc;
+	}
+
+	public String getApplytime() {
+		return applytime;
+	}
+
+	public void setApplytime(String applytime) {
+		this.applytime = applytime;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	@Override
+	public String toString() {
+		return "FestCustBoardDTO [custno=" + custno + ", applyname=" + applyname + ", applytel=" + applytel
+				+ ", applyno=" + applyno + ", applyloc=" + applyloc + ", applytime=" + applytime + ", hit=" + hit + "]";
+	}
+
+}
---
+++ project/src/com/foodtruck/festboard/dto/FestFirmBoardDTO.java
@@ -0,0 +1,131 @@
+package com.foodtruck.festboard.dto;
+
+public class FestFirmBoardDTO {
+
+	private int truckno;
+	private String country, maindish;
+	private int predppl, applyppl, readyfood;
+	private String mngrname, mngrtel;
+	private int hit;
+
+	// 초기화 블럭
+	public FestFirmBoardDTO() {
+
+	}
+
+	// 행사일정 리스트용
+	public FestFirmBoardDTO(int truckno, String country, String maindish, String mngrname, String mngrtel, int hit) {
+		this.truckno = truckno;
+		this.country = country;
+		this.maindish = maindish;
+		this.mngrname = mngrname;
+		this.mngrtel = mngrtel;
+		this.hit = hit;
+	}
+
+	// 행사일정 글보기용
+	public FestFirmBoardDTO(int truckno, String country, String maindish, int predppl, int applyppl, int readyfood,
+			String mngrname, String mngrtel, int hit) {
+		this.truckno = truckno;
+		this.country = country;
+		this.maindish = maindish;
+		this.predppl = predppl;
+		this.applyppl = applyppl;
+		this.readyfood = readyfood;
+		this.mngrname = mngrname;
+		this.mngrtel = mngrtel;
+		this.hit = hit;
+	}
+
+	// 행사일정 글등록 및 수정용
+	public FestFirmBoardDTO(String country, String maindish, int predppl, int applyppl, int readyfood, String mngrname,
+			String mngrtel) {
+		this.country = country;
+		this.maindish = maindish;
+		this.predppl = predppl;
+		this.applyppl = applyppl;
+		this.readyfood = readyfood;
+		this.mngrname = mngrname;
+		this.mngrtel = mngrtel;
+	}
+
+	public int getTruckno() {
+		return truckno;
+	}
+
+	public void setTruckno(int truckno) {
+		this.truckno = truckno;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getMaindish() {
+		return maindish;
+	}
+
+	public void setMaindish(String maindish) {
+		this.maindish = maindish;
+	}
+
+	public int getPredppl() {
+		return predppl;
+	}
+
+	public void setPredppl(int predppl) {
+		this.predppl = predppl;
+	}
+
+	public int getApplyppl() {
+		return applyppl;
+	}
+
+	public void setApplyppl(int applyppl) {
+		this.applyppl = applyppl;
+	}
+
+	public int getReadyfood() {
+		return readyfood;
+	}
+
+	public void setReadyfood(int readyfood) {
+		this.readyfood = readyfood;
+	}
+
+	public String getMngrname() {
+		return mngrname;
+	}
+
+	public void setMngrname(String mngrname) {
+		this.mngrname = mngrname;
+	}
+
+	public String getMngrtel() {
+		return mngrtel;
+	}
+
+	public void setMngrtel(String mngrtel) {
+		this.mngrtel = mngrtel;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	@Override
+	public String toString() {
+		return "FestFirmBoardDTO [truckno=" + truckno + ", country=" + country + ", maindish=" + maindish + ", predppl="
+				+ predppl + ", applyppl=" + applyppl + ", readyfood=" + readyfood + ", mngrname=" + mngrname
+				+ ", mngrtel=" + mngrtel + ", hit=" + hit + "]";
+	}
+
+}
