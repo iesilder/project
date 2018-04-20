e7cbc9412f60c07f20d7cfae153ea0fc131d33b1
hong <hong@401PC-B1>
2018-04-20 15:49:07 +0900

    encoding annotation 삭제

--- project/src/com/foodtruck/filter/EncodingFilter.java
+++ project/src/com/foodtruck/filter/EncodingFilter.java
@@ -8,12 +8,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 
 /**
  * Servlet Filter implementation class EncodingFilter
  */
-@WebFilter("/EncodingFilter")
 public class EncodingFilter implements Filter {
 
 	/**
