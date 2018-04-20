e853f2b4c330edb73984220af7845f6da79e3da5
hong <hong@401PC-B1>
2018-04-20 17:26:59 +0900

    festDAO 작성완료!

--- project/src/com/foodtruck/fest/dao/FestDAO.java
+++ project/src/com/foodtruck/fest/dao/FestDAO.java
@@ -1,6 +1,7 @@
 package com.foodtruck.fest.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -16,9 +17,9 @@ public class FestDAO {
 	// DBUtil에 다 선언함.
 
 	// 글리스트를 가져오는 메서드
-	public List<FestDAO> list(PageObject2 pgObj) {
-		System.out.println("BoardDAO.list()");
-		List<FestDAO> list = null;
+	public List<FestDTO> list(PageObject2 pgObj) {
+		System.out.println(getClass().getName() + ".list()");
+		List<FestDTO> list = null;
 		// RDBMS에서 데이터를 가져 오는 프로그램 작성
 
 		// 필요한 객체 선언
@@ -31,7 +32,7 @@ public class FestDAO {
 			con = DBUtil.getConnection();
 			// 3. sql
 			// 3-1. 원래 데이터를 순서에 맞게 가져온다.
-			String sql = "select festno, festname, to_char(festdate, 'yyyy-mm-dd') festdate, festloc, hit from FESTBOARD order by festno desc ";
+			String sql = "select festno, festname, to_char(festdate, 'yyyy-mm-dd') festdate, festloc, hit from festboard order by festno desc ";
 			// 3-2. 순서에 맞게 가져온 데이터에 rowNum을 붙인다.
 			sql = "select rownum rnum, festno, festname, festdate, festloc, hit from (" + sql + ")";
 			// 3-3. 페이지에 맞는 startRow, endRow를 설정한다.
@@ -53,7 +54,7 @@ public class FestDAO {
 				festDTO.setNo(rs.getInt("no"));
 				festDTO.setFestname(rs.getString("festname"));
 				festDTO.setFestdate(rs.getDate("festdate"));
-				festDTO.setWriteDate(rs.getString("writedate"));
+				festDTO.setFestloc(rs.getString("festloc"));
 				festDTO.setHit(rs.getInt("hit"));
 				// list에 festDTO를 담는다.
 				list.add(festDTO);
@@ -75,9 +76,9 @@ public class FestDAO {
 	}
 
 	// 글번호에 맞는 글보기 데이터를 가져오는 메서드
-	public FestDAO view(int no) {
-		System.out.println("BoardDAO.view()");
-		FestDAO festDTO = null;
+	public FestDTO view(int festno) {
+		System.out.println(getClass().getName() + ".view()");
+		FestDTO festDTO = null;
 		// 오라클에서 데이터를 가져와서 채우는 프로그램 작성(생략)
 		// 사용한 객체 선언
 		Connection con = null; // 연결 객체
@@ -87,21 +88,19 @@ public class FestDAO {
 			// 1. 드라이버 확인 //2. 연결
 			con = DBUtil.getConnection();
 			// 3. sql문 작성
-			String sql = "select no, title, content, " + " writer, writedate, hit " + " from board " + " where no = ? "; // 변하는
-																															// 데이터
-																															// 대신
-																															// ?
-																															// 사용
+			String sql = "select festno, festname, festcomp, to_char(festdate, 'yyyy-mm-dd') festdate, festloc, to_char(festtime, 'HH:mm')festtime, hit "
+					+ " from festboard " + " where festno = ? "; // 변하는
+			// 사용
 			// 4. 처리문 객체
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, no); // 첫번째 ?에 no를 int로 셋팅
+			pstmt.setInt(1, festno); // 첫번째 ?에 no를 int로 셋팅
 			// 5. 실행
 			rs = pstmt.executeQuery();
 			// 6. 표시 rs에서 꺼내서 FestDTO에 담는다.
 			if (rs.next()) {
 				// 생성자가 만들어져 있어야 한다.
-				festDTO = new FestDAO(rs.getInt("no"), rs.getString("title"), rs.getString("content"),
-						rs.getString("writer"), rs.getString("writeDate"), rs.getInt("hit"));
+				festDTO = new FestDTO(rs.getInt("festno"), rs.getString("festname"), rs.getString("festcomp"),
+						rs.getDate("festdate"), rs.getString("festloc"), rs.getDate("festtime"), rs.getInt("hit"));
 			}
 
 		} catch (Exception e) {
@@ -121,7 +120,7 @@ public class FestDAO {
 
 	// 게시판 글쓰기 처리.
 	public void write(FestDTO festDTO) {
-		System.out.println("BoardDAO.write()");
+		System.out.println(getClass().getName() + ".write()");
 		// 사용한 객체 선언
 		Connection con = null; // 연결 객체
 		PreparedStatement pstmt = null; // 처리문 객체
@@ -129,13 +128,15 @@ public class FestDAO {
 			// 1. 드라이버 확인 //2. 연결
 			con = DBUtil.getConnection();
 			// 3. sql문 작성
-			String sql = "insert into board(no,title," + " content, writer) " + " values(board_seq.nextval,"
-					+ " ?, ?, ?) "; // 변하는 데이터 대신 ? 사용
+			String sql = "insert into festboard(festno,festname, festcomp, to_char(festdate, 'yyyy-mm-dd')festdate, festloc, to_char(festtime, 'HH:mm')festtime) "
+					+ " values(festboard_seq.nextval," + " ?, ?, ?, ? ,?) "; // 변하는 데이터 대신 ? 사용
 			// 4. 처리문 객체
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, festDTO.getTitle());
-			pstmt.setString(2, festDTO.getContent());
-			pstmt.setString(3, festDTO.getWriter());
+			pstmt.setString(1, festDTO.getFestname());
+			pstmt.setString(2, festDTO.getFestcomp());
+			pstmt.setDate(3, (Date) festDTO.getFestdate());
+			pstmt.setString(4, festDTO.getFestloc());
+			pstmt.setDate(5, (Date) festDTO.getFesttime());
 			// 5. 실행 -> select: executeQuery()
 			// insert, update, delete:executeUpdate()
 			pstmt.executeUpdate();
@@ -155,8 +156,8 @@ public class FestDAO {
 	}
 
 	// 조회수를 1증가 시키는 메서드 -> 글번호를 받아서 글번호에 맞는 조회수 증가.
-	public void increase(int no) {
-		System.out.println("BoardDAO.increase()");
+	public void increase(int festno) {
+		System.out.println(getClass().getName() + ".increase()");
 		// 사용한 객체 선언
 		Connection con = null; // 연결 객체
 		PreparedStatement pstmt = null; // 처리문 객체
@@ -164,10 +165,10 @@ public class FestDAO {
 			// 1. 드라이버 확인 //2. 연결
 			con = DBUtil.getConnection();
 			// 3. sql문 작성
-			String sql = "update board set hit = hit + 1 " + " where no = ? "; // 변하는 데이터 대신 ? 사용
+			String sql = "update festboard set hit = hit + 1 where festno = ? "; // 변하는 데이터 대신 ? 사용
 			// 4. 처리문 객체
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, no); // 첫번째 ?에 no를 int로 셋팅
+			pstmt.setInt(1, festno); // 첫번째 ?에 no를 int로 셋팅
 			// 5. 실행 -> select: executeQuery()
 			// insert, update, delete:executeUpdate()
 			pstmt.executeUpdate();
@@ -188,7 +189,7 @@ public class FestDAO {
 
 	// 게시판 글수정 처리.
 	public void update(FestDTO festDTO) {
-		System.out.println("BoardDAO.update()");
+		System.out.println(getClass().getName() + ".update()");
 		// 사용한 객체 선언
 		Connection con = null; // 연결 객체
 		PreparedStatement pstmt = null; // 처리문 객체
@@ -196,14 +197,16 @@ public class FestDAO {
 			// 1. 드라이버 확인 //2. 연결
 			con = DBUtil.getConnection();
 			// 3. sql문 작성
-			String sql = "update board set " + " title = ?, content = ?, writer = ? " + " where no = ? "; // 변하는 데이터 대신
-																											// ? 사용
+			String sql = "update festboard set festname = ?, festcomp = ?, festdate = ?, festloc=?, festtime=? "
+					+ " where festno = ? "; // 변하는 데이터 대신
+			// ? 사용
 			// 4. 처리문 객체
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, festDTO.getTitle());
-			pstmt.setString(2, festDTO.getContent());
-			pstmt.setString(3, festDTO.getWriter());
-			pstmt.setInt(4, festDTO.getNo());
+			pstmt.setString(1, festDTO.getFestname());
+			pstmt.setString(2, festDTO.getFestcomp());
+			pstmt.setDate(3, (Date) festDTO.getFestdate());
+			pstmt.setString(4, festDTO.getFestloc());
+			pstmt.setDate(5, (Date) festDTO.getFesttime());
 			// 5. 실행 -> select: executeQuery()
 			// insert, update, delete:executeUpdate()
 			pstmt.executeUpdate();
@@ -223,8 +226,8 @@ public class FestDAO {
 	}
 
 	// 게시판 글삭제 처리
-	public void delete(int no) {
-		System.out.println("FestDTO.delete()");
+	public void delete(int festno) {
+		System.out.println(getClass().getName() + ".delete()");
 		// 사용한 객체 선언
 		Connection con = null; // 연결 객체
 		PreparedStatement pstmt = null; // 처리문 객체
@@ -232,10 +235,10 @@ public class FestDAO {
 			// 1. 드라이버 확인 //2. 연결
 			con = DBUtil.getConnection();
 			// 3. sql문 작성
-			String sql = "delete from board " + " where no = ?"; // 변하는 데이터 대신 ? 사용
+			String sql = "delete from festboard " + " where festno = ?"; // 변하는 데이터 대신 ? 사용
 			// 4. 처리문 객체
 			pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, no);
+			pstmt.setInt(1, festno);
 			// 5. 실행 -> select: executeQuery()
 			// insert, update, delete:executeUpdate()
 			pstmt.executeUpdate();
