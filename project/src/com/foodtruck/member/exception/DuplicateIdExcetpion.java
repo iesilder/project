f74f43b803a577c9e779510f50e0101570cf1b04
lyanglae <lyanglae@401PC-A1>
2018-04-24 13:56:18 +0900

    edited

---
+++ project/src/com/foodtruck/member/exception/DuplicateIdExcetpion.java
@@ -0,0 +1,14 @@
+package com.foodtruck.member.exception;
+
+public class DuplicateIdExcetpion extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DuplicateIdExcetpion() {
+		super("아이디가 중복되었습니다.");
+	}
+
+}
