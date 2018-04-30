9083e8c701b2f9efc3b346c26b587b08411e7558
TaeSun Yoo <yts8317@gmail.com>
2018-04-30 11:59:50 +0900

    Notice css빼고 끝

--- project/src/com/foodtruck/notice/service/NoticeDeleteService.java
+++ project/src/com/foodtruck/notice/service/NoticeDeleteService.java
@@ -5,7 +5,6 @@
 package com.foodtruck.notice.service;
 
 import com.foodtruck.notice.dao.NoticeDAO;
-import com.foodtruck.notice.dto.NoticeDTO;
 import com.foodtruck.util.ServiceInterface;
 
 public class NoticeDeleteService implements ServiceInterface {
@@ -21,12 +20,12 @@ public class NoticeDeleteService implements ServiceInterface {
 	@Override
 	public Object execute(Object obj) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println(getClass().getName() + "execute()");
+		System.out.println(getClass().getName() + ".execute()");
 
 		// 전달 받은 데이터를 DAO로 넘겨주기 위해서 Object를 캐스팅
-		NoticeDTO noticeDTO = (NoticeDTO) obj;
+		int no = (Integer) obj;
 		// DAO로 넘겨 삭제를 진행한다.
-		noticeDAO.delete(noticeDTO);
+		noticeDAO.delete(no);
 
 		return null;
 	}
